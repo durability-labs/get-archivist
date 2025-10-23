de82b44fdb93af1dc5fe5241ace228395884a249
Slava <20563034+veaceslavdoina@users.noreply.github.com>
2025-10-23 18:02:57 +0300

    chore(devnet): update marketplace related variables (#3)

--- devnet/run.sh
+++ devnet/run.sh
@@ -6,8 +6,8 @@ BASE_SCRIPT="https://get.archivist.storage/run.sh"
 SCRIPT_URL="https://get.archivist.storage/devnet/run.sh"
 ARCHIVIST_BINARY="${ARCHIVIST_BINARY:-./archivist}"
 BOOTSTRAP_NODE_FROM_URL="https://spr.archivist.storage/devnet"
-MARKETPLACE_ADDRESS_FROM_URL="https://marketplace.archivist.storage/archivist-devnet/latest"
-ARCHIVIST_ETH_PROVIDER="${ARCHIVIST_ETH_PROVIDER:-wss://public.sepolia.rpc.status.network/ws}"
+MARKETPLACE_ADDRESS_FROM_URL="${MARKETPLACE_ADDRESS_FROM_URL:-https://marketplace.archivist.storage/devnet/latest}"
+ARCHIVIST_ETH_PROVIDER="${ARCHIVIST_ETH_PROVIDER:-https://rpc.devnet.archivist.storage}"
 set +a
 
 # Help
